71cb28bd5144b216088512b1147892675b7035c5
Prernna <126183593+Prernna@users.noreply.github.com>
2023-04-04 14:03:26 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,15 @@
+public class ch4literals {
+    public static void main(String[] args) {
+
+        int age = 20;
+        int age2 = 10;
+        byte age = 28;
+        short age =16;
+        long age=47;
+        char gd = 'GD';
+        double s1= 19.00;
+        float s2 = 10.0f;
+        boolean ps = true;
+        System.out.println(age);
+    }
+}
